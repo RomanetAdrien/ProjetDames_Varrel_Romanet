abdc8b009f281320f2acb60cdfd6c361395702ce
Adrien Romanet <Adrien Romanet>
2017-03-15 17:39:33 +0100

    premiers tests realises

--- src/ProjetDames_Varrel_Romanet.java
+++ src/ProjetDames_Varrel_Romanet.java
@@ -7,16 +7,29 @@ public class ProjetDames_Varrel_Romanet {
 
 
     public static void main (String[] args){
+        int n= 4;
+        do {
+            Solution s = new Solution(n);
+            float best = s.fitness;
+            int i = 0;
+            do {
+                ArrayList<Solution> voisinage = s.getVoisinage();
 
-        Solution s = new Solution(8);
-        s.printSolution();
-
-        ArrayList<Solution> voisinage = s.getVoisinage();
-
-        for (Solution ss : voisinage) {
-            ss.printSolution();
-        }
-
+                for (Solution ss : voisinage) {
+                    if (ss.fitness <= best) {
+                        s = ss;
+                        best = ss.fitness;
+                    }
+                }
+                i++;
+            } while (i < 300 && best > 0);
+            if(best==0) {
+                System.out.println(n);
+                s.printSolution();
+                System.out.println("Itérations : " + i);
+            }
+            n++;
+        }while(n<100);
 
     }
 
